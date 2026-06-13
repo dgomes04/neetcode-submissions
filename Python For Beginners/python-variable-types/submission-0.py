9e29893a615f325c2c18edd58f181f6c386ceeca
Davi Gomes <gdavi989@gmail.com>
2026-06-13 09:28:16 -0300

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,10 @@
+integer_type = 1
+float_type = 98.5
+bool_type = False
+string_type = "Teste"
+list_type = [1,2,3]
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
