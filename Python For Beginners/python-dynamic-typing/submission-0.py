db9361a262d134490fa02de589c52374a89dbdaa
Davi Gomes <gdavi989@gmail.com>
2026-06-13 09:30:09 -0300

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 1
+print(type(variable))
+variable = 89.8
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Hello, World!"
+print(type(variable))
+variable = [1,2,3]
+print(type(variable))
